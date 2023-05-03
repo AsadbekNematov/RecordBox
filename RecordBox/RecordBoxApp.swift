cbd6d3f5cdb09b5d97b88dbee7100ed553d86815
Asadbek <asadbekn443@gmail.com>
2023-05-03 02:26:14 -0400

    Initial Commit

---
+++ RecordBox/RecordBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordBoxApp.swift
+//  RecordBox
+//
+//  Created by Asadbek Nematov on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
